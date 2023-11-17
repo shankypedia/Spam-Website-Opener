cc6f5bccd2552f77e231c619768a0a6500027461
Sashank <sashank@warspion.com>
2023-11-17 18:37:31 +0530

    Update SWO.py
    - Wrapped the code in functions for better structure.
    - The program automatically stops after opening a specified number of websites (100 by default).
    - Provided a way for the user to terminate the program using Ctrl + C.
    - Added comments for better understanding.

--- SWO.py
+++ SWO.py
@@ -2,15 +2,42 @@ import webbrowser
 import time
 import random
 
-while True:
-    sites = sorted([
+def open_random_website():
+    # List of websites for developers
+    websites = [
         'coderanch.com', 'python.org', 'quora.com', 'nodejs.org', 'github.com',
         'stackoverflow.com', 'stackexchange.com', 'learn-anything.xyz',
-        'devrant.com', 'google.com', 'codementor.io', 'codewars.com'
-    ])
-    
-    visit = f"https://{random.choice(sites)}"
+        'devrant.com', 'google.com', 'codementor.io', 'codewars.com',
+        'w3schools.com', 'hackerrank.com', 'freeCodeCamp.org',
+        'developer.mozilla.org', 'codecademy.com', 'git-scm.com',
+        'visualstudio.com', 'docker.com', 'npmjs.com', 'ruby-lang.org',
+        'jetbrains.com', 'dzone.com', 'css-tricks.com', 'heroku.com',
+        'golang.org', 'kaggle.com', 'leetcode.com', 'dev.to', 'hashnode.com'
+    ]
+
+    selected_website = random.choice(websites)
+    print(f"Opening: {selected_website}")
+    visit = f"https://{selected_website}"
     webbrowser.open(visit)
 
-    seconds = random.randrange(5, 10)
-    time.sleep(seconds)
+def main():
+    try:
+        # Set the limit for the number of website openings
+        limit_openings = 100
+        openings_count = 0
+
+        while openings_count < limit_openings:
+            open_random_website()
+            seconds = random.uniform(5, 10)
+            time.sleep(seconds)
+
+            # Increment the counter after opening a website
+            openings_count += 1
+
+        print("Program stopped after opening 100 websites.")
+
+    except KeyboardInterrupt:
+        print("\nProgram terminated by user.")
+
+if __name__ == "__main__":
+    main()
